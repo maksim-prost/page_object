afb0a2dc2f750f755a6b282672b266261071c8c9
maksim-prost <zendzin@list.ru>
2021-08-11 21:23:10 +0500

    Ожидание появления элементов

--- pages/locators.py
+++ pages/locators.py
@@ -1,3 +1,4 @@
+from typing import cast
 from selenium.webdriver.common.by import By
 
 
@@ -7,3 +8,11 @@ class MainPageLocators():
 class LoginPageLocators():
     FORM_LOGIN = (By.ID, "login_form")
     FORM_REGISTER = (By.ID, "register_form")
+
+class AddBasketPageLocators():
+    BUTTON_BASKET = ( By.CLASS_NAME, "btn-add-to-basket")
+    MESSAGE_NAME_PRODUCT = ( By.CSS_SELECTOR, ".alertinner strong")
+    PRICE_BASKET = ( By.CSS_SELECTOR,".alert-info strong")
+    NAME_PRODUCT = ( By.CSS_SELECTOR, ".product_main  h1")
+    PRICE_PRODUCT = ( By.CSS_SELECTOR, ".product_main  p")
+    MESSAGE_SUCCES = ( By.CLASS_NAME, 'alert-success')
--- test_product_page.py
+++ test_product_page.py
@@ -1,14 +1,36 @@
 from .pages.product_page import PageProduct
+from .pages.locators import AddBasketPageLocators
 import pytest
 
-
-@pytest.mark.parametrize("link", 
-[f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{i}" for i in range(10) 
-            if i != 7] + [ pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail) ])
-def test_guest_can_go_to_basket_page(browser, link):
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+# @pytest.mark.parametrize("link", 
+# [f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{i}" for i in range(10) 
+#             if i != 7] + [ pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail) ])
+# def test_guest_can_go_to_basket_page(browser, link):
+#     page = PageProduct (browser, link) 
+#     page.open() 
+#     page.add_product_to_basket()
+   
+@pytest.mark.xfail 
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = PageProduct (browser, link) 
     page.open() 
     page.add_product_to_basket()
-    # page.solve_quiz_and_get_code()
+    assert page.is_not_element_present(*AddBasketPageLocators.MESSAGE_SUCCES), \
+         "Success message is presented, but should not be"
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = PageProduct (browser, link) 
+    page.open() 
+    assert page.is_not_element_present(*AddBasketPageLocators.MESSAGE_SUCCES), \
+         "Success message is presented, but should not be"
+
+@pytest.mark.xfail 
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = PageProduct (browser, link) 
+    page.open() 
+    page.add_product_to_basket()
+    assert page.is_disappeared(*AddBasketPageLocators.MESSAGE_SUCCES), \
+         "Success message is presented, but should not be"
